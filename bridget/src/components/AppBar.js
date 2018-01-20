923a9284d6a81956af4435fe8e87a729bd1468b8
Ronald Rounsifer <ronrounsifer@me.com>
2018-01-20 11:40:00 -0500

    Begin fuxxing with UI

--- bridget/src/components/AppBar.js
+++ bridget/src/components/AppBar.js
@@ -4,12 +4,20 @@ import './AppBar.css';
 class AppBar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg nav-bg">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navbar">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <span className="navbar-text">Bridget</span>
-      </nav>
+      <div className="pos-f-t">
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="nav-bg p-4">
+            <h4 className="text-white">Collapsed content</h4>
+            <span className="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav className="navbar nav-bg">
+          <button className="btn deal-btn" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            Deals
+          </button>
+          <span className="navbar-text">Bridget</span>
+        </nav>
+      </div>
     );
   }
 }
